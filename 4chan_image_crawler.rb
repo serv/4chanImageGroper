fa40489f7ca25d2e2bce71aaf9aea648ad600d29
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-02-15 21:46:54 -0500

    new file naming system

--- 4chan_image_crawler.rb
+++ 4chan_image_crawler.rb
@@ -5,9 +5,11 @@ def urlToString(url)
   return data
 end
 
-def writeToFile(image_url, i)
+def writeToFile(image_url)
   require 'open-uri'
-  open("images/#{i}.jpg", 'wb') do |file|
+  file_name = image_url[0][31..48]
+  puts image_url[0][31..48]
+  open("images/#{file_name}", 'wb') do |file|
     file << open(image_url.first).read
   end
 end
@@ -16,22 +18,25 @@ def main(url)
   html_content = urlToString(url)
   occurances = html_content.scan(/([http]{4}...[images]{6}.[4chan]{5}.[org]{3}.[b]{1}.[src]{3}.[0-9]{13}.[jpg|png|jpeg|gif]{3,4})/)
   result = []
-  puts occurances
-  (0..occurances.count).each do |i|
+  (0..occurances.count-1).each do |i|
     if i%2 == 0
       result << occurances[i]
     end
   end
-  puts "dfasdgas"
   puts result
+  puts "There are #{result.count} images to download."
   
   (0..result.count).each do |i|
-    writeToFile(result[i], i)
+    if result[i] != nil
+      puts i
+      writeToFile(result[i])
+    end
+    sleep 2 #seconds
   end
   
   # result.each do |image_url|
   #     writeToFile(image_url)
-  #   end
+  # end
 end
   
 ARGV.each do |url|
